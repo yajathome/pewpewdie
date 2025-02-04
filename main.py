fb24df55b621a327f2e598af1d832e89151221e5
Pranshu2010 <pranshthaker@gmail.com>
2025-02-04 17:18:47 +0530

    googoogaagaa

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/buy')
+def temp():
+    return render_template('by page.html')
 if __name__ == '__main__':
     app.run(debug=True)
